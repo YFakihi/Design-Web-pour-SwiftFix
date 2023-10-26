aca9e3049d02e8690bfc62147f23b94fdb929940
yfakihi <fakihiyassin01@gmail.com>
2023-10-26 11:16:00 +0100

    dons ce parier j ai ajouter Testimonial

---
+++ testimonial.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded',function(){
+const carousl = document.querySelector('.Testimonials')
+const item = document.querySelectorAll('.Text_testimonial1')
+let width = item[0].clientWidth
+let index = 0
+function carousel(){
+    index = (index + 1) % item.length
+    let trans = -index * width
+    carousl.style.transform = `translateX(${trans}px)`
+}
+setInterval(carousel,3000)
+
+})
